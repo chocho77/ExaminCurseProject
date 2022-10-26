4fd72d5a3992e387bfe07af3850554420db1872e
chavdar77 <chavmom@gmail.com>
2022-10-26 17:20:55 +0300

    Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   src/models/TypeOfResearch.java

--- src/models/TypeOfResearch.java
+++ src/models/TypeOfResearch.java
@@ -1,4 +1,32 @@
 package models;
 
 public class TypeOfResearch {
+
+    private String nameOfResearch;
+    private double priceOfResearch;
+
+    public TypeOfResearch(){
+
+    }
+
+    public TypeOfResearch(String nameOfResearch, double priceOfResearch) {
+        this.nameOfResearch = nameOfResearch;
+        this.priceOfResearch = priceOfResearch;
+    }
+
+    public String getNameOfResearch() {
+        return nameOfResearch;
+    }
+
+    public void setNameOfResearch(String nameOfResearch) {
+        this.nameOfResearch = nameOfResearch;
+    }
+
+    public double getPriceOfResearch() {
+        return priceOfResearch;
+    }
+
+    public void setPriceOfResearch(double priceOfResearch) {
+        this.priceOfResearch = priceOfResearch;
+    }
 }
