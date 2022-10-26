a864a7ff94a873573b5c217250d42bd7ad64ae9a
chavdar77 <chavmom@gmail.com>
2022-10-26 17:36:05 +0300

    Changes to be committed:  	modified:   .idea/workspace.xml 	modified:   src/models/MedicalTechnologist.java 	modified:   src/models/Pacient.java

--- src/models/MedicalTechnologist.java
+++ src/models/MedicalTechnologist.java
@@ -1,2 +1,31 @@
-package models;public class MedicalTechnologist {
+package models;
+
+public class MedicalTechnologist {
+
+    private String name;
+    private TypeOfResearch typeOfResearch;
+
+    public MedicalTechnologist() {
+    }
+
+    public MedicalTechnologist(String name, TypeOfResearch typeOfResearch) {
+        this.name = name;
+        this.typeOfResearch = typeOfResearch;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TypeOfResearch getTypeOfResearch() {
+        return typeOfResearch;
+    }
+
+    public void setTypeOfResearch(TypeOfResearch typeOfResearch) {
+        this.typeOfResearch = typeOfResearch;
+    }
 }
--- src/models/Pacient.java
+++ src/models/Pacient.java
@@ -1,2 +1,40 @@
-package models;public class Pacient {
+package models;
+
+public class Pacient {
+
+    private String name;
+    private double healthInsuranceAmount;
+
+    public Pacient() {
+    }
+
+    public Pacient(String name, double healthInsuranceAmount) {
+        this.name = name;
+        this.healthInsuranceAmount = healthInsuranceAmount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHealthInsuranceAmount() {
+        return healthInsuranceAmount;
+    }
+
+    public void setHealthInsuranceAmount(double healthInsuranceAmount) {
+        this.healthInsuranceAmount = healthInsuranceAmount;
+    }
+
+    public String getPacientInfo() {
+
+        String returnInfoPacient  = "Pacient name : " + this.name + " health Insurance Amount : " + this.healthInsuranceAmount;
+
+        return returnInfoPacient;
+
+    }
+
 }
